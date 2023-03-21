4922dcfa7b58baf7592f5632da7d453560bcfa3c
Ramon Perez <ramonpzg@protonmail.com>
2023-03-21 22:52:08 +1100

    final version of load

--- src/load.py
+++ src/load.py
@@ -1,19 +1,14 @@
 
 from pathlib import Path
 import pandas as pd
-from extract import load_table
 import ibis
+from os.path import join
 
 def create_db(path_in, path_out, file_name, table_name):
     path = Path(path_out)
     conn = ibis.duckdb.connect(path.joinpath(file_name))
     conn.register(path_in, table_name=table_name)
     print(f"Successfully loaded the {table_name} table!")
-
-def create_parquet(path_in, path_out, file_name):
-    path = Path(path_out)
-    pd.read_parquet(path_in).to_parquet(path.joinpath(file_name))
-    print(f"Successfully loaded the {file_name} table!")
 
 def save_data(data, path_out, file_name):
     path_out = Path(path_out)
@@ -23,7 +18,7 @@ def save_data(data, path_out, file_name):
     
 if __name__ == "__main__":
     create_db(
-        path_in=join("data", "processed", "transformed_data.parquet"),
+        path_in=join("data", "processed", "combined_table.parquet"),
         path_out=join("data", "dwarehouse"),
         file_name="db_analytics.ddb",
         table_name="truth_preds_challenge"
