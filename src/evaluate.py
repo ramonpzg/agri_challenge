8b2ea17e0569f99c2240c9b49c58a0a91151d2eb
Ramon Perez <ramonpzg@protonmail.com>
2023-03-21 22:52:23 +1100

    final version of evaluate

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,12 +1,43 @@
-from sklearn import metrics as m
+
+import sklearn.metrics as m
+import pandas as pd
+from os.path import join
+from omegaconf import OmegaConf
 from extract import load_table
 
-def get_metrics(data, y_truth, y_pred):
-    y_truth = data['y_truth'].values
-    y_preds = data['y_pred'].values
+def get_metrics(df, y_truth, y_pred, label):
+    return dict(
+        precision=round(m.precision_score(df[y_truth], df[y_pred], pos_label=label) * 100, 2),
+        recall=round(m.recall_score(df[y_truth], df[y_pred], pos_label=label) * 100, 2),
+        f1_score=round(m.f1_score(df[y_truth], df[y_pred], pos_label=label) * 100, 2),
+        accuracy=round(m.accuracy_score(df[y_truth], df[y_pred]) * 100, 2)
+    )
+
+def confused_mtx(df, y_truth, y_pred):
+    return m.confusion_matrix(df[y_truth], df[y_pred])
+
+def generate_config(metrics, mtx, path, file_name):
+    conf = OmegaConf.create({
+        "facts": {
+            "sample_size": int(sum(sum(mtx))),
+            "healthy_preds": int(mtx[0][0]),
+            "wrong_preds": int(mtx[1][0] - mtx[0][1])
+        },
+        "metrics": {k: float(v) for k, v in metrics.items()},
+        "matrix": {
+            "true_pos": int(mtx[0][0]),
+            "true_neg": int(mtx[1][1]),
+            "false_pos": int(mtx[0][1]),
+            "false_neg": int(mtx[1][0])
+        }
+    })
     
-    accuracy = m.accuracy_score
-    precision = m.accuracy_score
-    f1_score = m.accuracy_score
-    recall = m.accuracy_score
-    specificity = m.accuracy_score
+    OmegaConf.save(conf, join(path, file_name))
+    
+    print(f"Config Successfully saved as {join(path, file_name)}")
+
+if __name__ == "__main__":
+    df = load_table(join("data", "processed"), "combined_table.parquet")
+    metrics = get_metrics(df, "class_truth", "class_pred", 'Alternaria spp.')
+    conf_mtx = confused_mtx(df, "class_truth", "class_pred")
+    generate_config(metrics, conf_mtx, join("src", "configs"), "config.yml")
