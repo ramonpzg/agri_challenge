a14c81d1a045d13ef15835d2e711486d20ed75e5
Ramon Perez <ramonpzg@protonmail.com>
2023-03-22 11:11:06 +1100

    added description to page 2

--- src/app/pages/3_Part_2_Walkthrough_🚶.py
+++ src/app/pages/3_Part_2_Walkthrough_🚶.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import streamlit as st
 
 st.sidebar.markdown("# Notebook for Programming Project")
+st.sidebar.markdown("This is a markdown version of the exploration notebook found on the root directory of this poject.")
 
 exploration_nb = Path("exploration.md").read_text()
 
